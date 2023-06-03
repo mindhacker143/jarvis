586bfc23d5ec7bf7d330205839e951ef699f2ae2
mindhacker143 <mohammadaltaf9418@gmail.com>
2023-06-03 07:54:34 +0530

    button

--- src/button1.tsx
+++ src/button1.tsx
@@ -4,7 +4,7 @@ function BUTTON() {
   return (
     <div className=' space-x-6  flex text-center items-center'>
         <a href="" className='text-[13px] text-white hover:text-slate-200 font-bold'>Sign up</a>
-        <button className=' rounded-none w-[152px] h-[58px] text-[13px] bg-[#FF4820] text-white  border-2 hover:border-white hover:bg-transparent hover:text-white font-bold '>Sign in</button>
+        <button className=' rounded-none w-[152px] h-[58px] text-[13px] bg-[#FF4820] text-white  hover:border-2 hover:border-white hover:bg-transparent hover:text-white font-bold '>Sign in</button>
         </div>
   )
 }
